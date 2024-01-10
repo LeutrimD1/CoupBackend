0810c3c02a6bd56c6168ed345353628ef32618e4
Leutrim Dema <leutrimd1@gmail.com>
2024-01-09 22:58:13 -0600

    remove

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import redis
 
 # Connect to Redis
-r = redis.from_url('rediss://red-cmf1m5qcn0vc73buq2e0:KM5M3jT3x8Ndz5NsLcmyeCdWHd66RKCM@oregon-redis.render.com:6379')
+r = redis.from_url('')
 
 # Set a key-value pair
 # r.set('mykey', 'Hello, Redis!')
