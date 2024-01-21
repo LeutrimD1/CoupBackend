f63c8939b7581860e529289b7e7c3ff718572d1e
Leutrim Dema <leutrimd1@gmail.com>
2024-01-20 19:21:37 -0600

    code changes

--- start.py
+++ start.py
@@ -4,5 +4,3 @@ import sys
 # Install Flask
 subprocess.call([sys.executable, '-m', 'pip', 'install', 'flask'])
 
-# Execute the main program (program.py)
-subprocess.call([sys.executable, 'program.py'])
