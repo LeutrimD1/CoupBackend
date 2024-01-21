873008434e4c73dbc2263d58463bea0472860bef
Leutrim Dema <leutrimd1@gmail.com>
2024-01-20 20:15:46 -0600

    go backwards

--- program.py
+++ program.py
@@ -1,24 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/endpoint1', methods=['POST'])
-def endpoint1():
-    data = request.json
-    parameter = data.get('parameter', None)
-    return jsonify({'result': 'Endpoint 1', 'parameter': parameter})
-
-@app.route('/endpoint2', methods=['POST'])
-def endpoint2():
-    data = request.json
-    parameter = data.get('parameter', None)
-    return jsonify({'result': 'Endpoint 2', 'parameter': parameter})
-
-@app.route('/endpoint3', methods=['POST'])
-def endpoint3():
-    data = request.json
-    parameter = data.get('parameter', None)
-    return jsonify({'result': 'Endpoint 3', 'parameter': parameter})
+@app.route('/')
+def hello():
+    return 'Hello, World! This is a Flask app running on Render.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
