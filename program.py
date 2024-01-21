7ef8d7970ebbd02e9e9b3c74ff5d4dcd108a6968
Leutrim Dema <leutrimd1@gmail.com>
2024-01-20 19:44:25 -0600

    code changes

--- program.py
+++ program.py
@@ -21,4 +21,4 @@ def endpoint3():
     return jsonify({'result': 'Endpoint 3', 'parameter': parameter})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
