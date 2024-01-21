dba4defb29b263a8e0435c5f6f17768a88ecab0b
Leutrim Dema <leutrimd1@gmail.com>
2024-01-21 00:52:16 -0600

    code changes

--- start.py
+++ start.py
@@ -5,3 +5,5 @@ import sys
 subprocess.call([sys.executable, '-m', 'pip', 'install', 'flask'])
 
 subprocess.call([sys.executable, '-m', 'pip', 'install', 'waitress'])
+
+subprocess.call([sys.executable, '-m', 'pip', 'install', 'flask_cors'])
